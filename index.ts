136a53cc1a525bcb0731bc080f209e2ccc557caa
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-10-22 15:04:19 +0200

    test

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ function handlePreFlightRequest(): Response {
 	  handlePreFlightRequest();
 	}
 
-	return new Response(JSON.stringify(_req.url), {
+	return new Response(JSON.stringify({value: 0}), {
 	status: 200,
 	headers: {
 		"Content-Type": "application/json",
